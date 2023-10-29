b58792102db8c843518a7777468d03d99b193cd2
geetansh22 <geetanshdwivedi1@gmail.com>
2023-10-30 00:00:04 +0530

    version-1.1.0

--- app/src/main/java/com/example/socialapp/CreateProfileActivity.kt
+++ app/src/main/java/com/example/socialapp/CreateProfileActivity.kt
@@ -13,64 +13,114 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 
 class CreateProfileActivity : AppCompatActivity() {
-    private lateinit var pickImage:ImageButton
-    private  var imageUri2:Uri?=null
-    private val db = Firebase.firestore
-    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data: Intent? = result.data
-            if (data != null) {
-                val selectedImage: Uri? = data.data
-                selectedImage?.let {
-                    imageUri2=selectedImage
-                    Glide.with(this)
-                        .load(selectedImage)
-                        .into(pickImage)
+    val storage = FirebaseStorage.getInstance()
+    val storageRef = storage.reference
+    private lateinit var pickImage: ImageButton
+    private var imageUri2: Uri? = null
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null) {
+                    val selectedImage: Uri? = data.data
+                    selectedImage?.let {
+                        imageUri2 = selectedImage
+                        Glide.with(this)
+                            .load(selectedImage)
+                            .into(pickImage)
+                    }
                 }
             }
         }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
 
-        val email=intent.getStringExtra("userEmail")
-        pickImage=findViewById(R.id.pickImage)
-        pickImage.setOnClickListener{
+        val email = intent.getStringExtra("userEmail")
+        pickImage = findViewById(R.id.pickImage)
+        pickImage.setOnClickListener {
             imagePicker()
         }
-        val userName=findViewById<EditText>(R.id.userName)
-        val userName2=userName.text.toString()
-        val createButton=findViewById<Button>(R.id.createButton)
-        createButton.setOnClickListener{
-            addUser(email,userName2,imageUri2)
-            val intent=Intent(this,MainActivity::class.java)
-            startActivity(intent)
+        val createButton = findViewById<Button>(R.id.createButton)
+        createButton.setOnClickListener {
+            val userName = findViewById<EditText>(R.id.userName)
+            val userName2 = userName.text.toString()
+
+            if (imageUri2 != null) {
+                // Generate a unique filename for the image (e.g., using the user's ID)
+                val userId = FirebaseAuth.getInstance().currentUser?.uid
+                val imageFileName = "profile_images/$userId.jpg"
+
+                // Get a reference to the Firebase Storage location
+                val imageRef = storageRef.child(imageFileName)
+
+                // Upload the selected image to Firebase Storage
+                imageRef.putFile(imageUri2!!)
+                    .addOnSuccessListener {
+                        // Image uploaded successfully, now add user data to Firestore
+                        val downloadUrlTask = imageRef.downloadUrl
+                        downloadUrlTask.addOnSuccessListener { uri ->
+                            addUser(email, userName2, uri)
+                            val intent=Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        // Handle errors during image upload
+                        Log.e("FirebaseStorageError", "Image upload failed", e)
+                        Toast.makeText(
+                            this,
+                            "Image upload failed: ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            } else {
+                // Handle case when no image is selected
+                Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
-    private fun imagePicker(){
-        val intent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+    private fun imagePicker() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         getContent.launch(intent)
     }
-    private fun addUser(email:String?,userName:String?,pickImage:Uri?){
-        val user = hashMapOf(
-            "email" to email,
-            "username" to userName,
-            "image_url" to pickImage?.toString()
-        )
-        db.collection("users").add(user)
-            .addOnSuccessListener { documentReference ->
-                val message = "Profile Created with ID: " + documentReference.id
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener { e ->
-                Log.e("FirestoreError", "Firestore operation failed", e)
-                Toast.makeText(this, " ${e.message}", Toast.LENGTH_SHORT).show()
-            }
 
+    private fun addUser(email: String?, userName: String?, imageUri: Uri?) {
+        val db = Firebase.firestore
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        if (userId != null) {
+            val user = hashMapOf(
+                "email" to email,
+                "username" to userName,
+                "userImage" to imageUri.toString()
+            )
+
+            db.collection("users").document(userId)
+                .set(user)
+                .addOnSuccessListener {
+                    val message = "Profile Created with UID: $userId"
+                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.e("FirestoreError", "Firestore operation failed", e)
+                    Toast.makeText(
+                        this,
+                        "Firestore operation failed: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        } else {
+            Toast.makeText(this, "User is not authenticated", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/socialapp/MainActivity.kt
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -1,19 +1,45 @@
 package com.example.socialapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter:PostAdapter
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val query = db.collection("newPost").orderBy("currentTime", Query.Direction.DESCENDING)
+        val options = FirestoreRecyclerOptions.Builder<Post>()
+            .setQuery(query, Post::class.java)
+            .build()
+        adapter= PostAdapter(options)
+        val recyclerView=findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager= LinearLayoutManager(this)
+        recyclerView.adapter=adapter
        val addPost=findViewById<FloatingActionButton>(R.id.addPost)
         addPost.setOnClickListener{
             val intent=Intent(this,CreatePost::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
 }
